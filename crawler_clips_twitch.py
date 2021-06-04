a8744c3bd0d1cafc41a7e092e7f5f64ab9a0ba52
biston <biston.nunes@gmail.com>
2021-06-04 11:33:14 -0300

    feat: last stable version

--- crawler_clips_twitch.py
+++ crawler_clips_twitch.py
@@ -1,26 +1,27 @@
+from typing import Tuple
 from selenium import webdriver
 import requests
 import os
 import time
 from moviepy.editor import VideoFileClip, concatenate_videoclips
-
 class VideoTwitch(object):
     # url = 'https://www.twitch.tv/directory/game/League%20of%20Legends/clips?range=7d'
     # video_time = 2
     # video_name = 'teste'
 
-    url = input('URL dos clips: ')
-    video_time = int(input('Tempo maximo em minutos do video: '))
-    video_name = input('Nome do video: ')
-
-    driver = webdriver.Chrome('./webdriver/chromedriver')
-    driver.get(url)
-    time.sleep(1.5)
-
     def __init__(self):
+        self.url = input('URL dos clips: ')
+        self.video_time = int(input('Tempo maximo em minutos do video: '))
+        self.video_name = input('Nome do video: ')
+    
+        self.driver = webdriver.Chrome('./webdriver/chromedriver')
+        self.driver.get(self.url)
+        time.sleep(2)
+
         self.video_time = self.video_time * 60
         has_language = input('Deseja setar um filtro por lingua? [N/y]: ')
         self.setLanguageTwitch(has_language)
+
         clips_array = self.createClipsArray()
         self.downloadClips(clips_array)
         self.driver.close()
@@ -29,23 +30,26 @@ class VideoTwitch(object):
     def createClipsArray(self):
         clips = []
         current_video_duration = 0
-        #descer a pagina
+        
+        self.downPage()
+
         videos_array = self.driver.find_elements_by_css_selector('div[data-a-target|=clips-card]')
+        
         for idx, video in enumerate(videos_array):
             clips.append({
                 'name': video.find_element_by_css_selector('h3').text,
                 'channel': video.find_element_by_css_selector('p').text,
-                'duration': video.find_elements_by_css_selector('div.tw-top-0 div p')[2].text,
-                'create_by': video.find_elements_by_css_selector('div.tw-top-0 div p')[1].text.split(' ')[-1],
-                'views': video.find_element_by_css_selector('div.tw-bottom-0.tw-left-0').text,
-                'url': video.find_element_by_css_selector('div.tw-item-order-1 div div a').get_attribute('href')
+                'duration': video.find_elements_by_css_selector('div div p')[2].text,
+                'create_by': video.find_elements_by_css_selector('div div p')[1].text.split(' ')[-1],
+                'views': video.find_elements_by_css_selector('div div p')[3].text,
+                'url': video.find_element_by_css_selector('div div div a').get_attribute('href')
             })
             current_video_duration = current_video_duration + self.durationInSeconds(clips[idx]['duration'])  
 
             if current_video_duration >= self.video_time:
                 return clips
 
-    def durationInSeconds(self, _duration:str):
+    def durationInSeconds(self, _duration: str):
         _duration = _duration.split(':')
         return (int(_duration[0]) * 60) + int(_duration[1])
 
@@ -59,7 +63,7 @@ class VideoTwitch(object):
 
         for idx, clip in enumerate(_clips_array):
             self.driver.get(clip['url'])
-            time.sleep(1.5)
+            time.sleep(2)
             link_video_download = self.driver.find_element_by_xpath(xpath_clip).get_attribute('src')
             file_download = requests.get(link_video_download, allow_redirects=True)
             open('./clips/{}/{}{}.mp4'.format(self.video_name, self.video_name, idx), 'wb').write(file_download.content)
@@ -67,13 +71,19 @@ class VideoTwitch(object):
     def concatenateClips(self, _clips_array):
         concatenate_array = []
         for idx in range(len(_clips_array)):
-            concatenate_array.append(VideoFileClip('./clips/{}/{}{}.mp4'.format(self.video_name, self.video_name, idx)).resize(width=1920))
+            concatenate_array.append(
+                VideoFileClip('./clips/{}/{}{}.mp4'.format(self.video_name, self.video_name, idx)).resize(width=1920)
+                )
+            concatenate_array.append(
+                VideoFileClip('./clips/tran_video.mp4').resize(width=1920)
+                )
         final_clip = concatenate_videoclips(concatenate_array)
-        final_clip.write_videofile("./clips/{}/{}-conc.mp4".format(self.video_name, self.video_name))
+        final_clip.write_videofile('./clips/{}/{}-conc.mp4'.format(self.video_name, self.video_name))
     
     def setLanguageTwitch(self, _has_language):
         if _has_language == 'y' or _has_language == 'Y':
-            self.driver.find_element_by_css_selector('div div div div button[data-test-selector=language-select-menu__toggle-button]').click()
+            language_button = self.driver.find_elements_by_css_selector('div[data-toggle-balloon-id]')[1]
+            language_button.click()
             print('Selecione o filtro de lingua: ')
 
             array_languages = self.driver.find_elements_by_css_selector('div[data-language-code]')
@@ -81,7 +91,7 @@ class VideoTwitch(object):
                 print('Selecione {} para {}'.format(idx, array_languages[idx].text))
 
             id_language = int(input(''))
-            selected_language = array_languages[id_language].click()
+            array_languages[id_language].click()
 
         elif _has_language == 'N' or _has_language == 'n' or _has_language == '':
             return
@@ -89,3 +99,18 @@ class VideoTwitch(object):
         else:
             has_language = input('Deseja setar um filtro por lingua? [N/y]')
             self.setLanguageTwitch(has_language)
+    
+    def downPage(self):
+        SCROLL_PAUSE_TIME = 0.5
+
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
--- index.py
+++ index.py
@@ -1,41 +1,41 @@
 # from crawler_clips_twitch import VideoTwitch
 import pandas as pd
-from google_services import Create_Service, convert_to_RFC_datetime
+from google_services import Create_Service
 from googleapiclient.http import MediaFileUpload
 from datetime import datetime
+from crawler_clips_twitch import VideoTwitch
 
-CLIENT_SECRET_FILE = './credentials/client_secret.json'
-API_NAME = 'youtube'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+VideoTwitch()
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-upload_date_time = datetime.utcnow().isoformat() + 'Z'
+# CLIENT_SECRET_FILE = './credentials/client_secret.json'
+# API_NAME = 'youtube'
+# API_VERSION = 'v3'
+# SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
-request_body = {
-    'snippet': {
-        'categoryI': 19,
-        'title': 'Upload Testing',
-        'description': 'Hello World Description',
-        'tags': ['Travel', 'video test', 'Travel Tips']
-    },
-    'status': {
-        'privacyStatus': 'public',
-        'publishAt': upload_date_time,
-        'selfDeclaredMadeForKids': False, 
-    },
-    'notifySubscribers': True
-}
+# service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+# upload_date_time = datetime(2021, 5, 9, 0, 0, 0).isoformat() + '.000Z'
 
-mediaFile = MediaFileUpload('./clips/my_concatenation.mp4')
+# request_body = {
+#     'snippet': {
+#         'categoryI': 19,
+#         'title': 'Upload Testing',
+#         'description': 'Hello World Description',
+#         'tags': ['Travel', 'video test', 'Travel Tips']
+#     },
+#     'status': {
+#         'privacyStatus': 'public',
+#         'selfDeclaredMadeForKids': False, 
+#     },
+#     'notifySubscribers': True
+# }
 
-# videotwitch = VideoTwitch()
+# mediaFile = MediaFileUpload('./clips/{}/{}-conc.mp4'.format(video_name, video_name))
 
-response_upload = service.videos().insert(
-    part='snippet,status',
-    body=request_body,
-    media_body=mediaFile
-).execute()
+# response_upload = service.videos().insert(
+#     part='snippet,status',
+#     body=request_body,
+#     media_body=mediaFile
+# ).execute()
 
 # service.thumbnails().set(
 #     videoId=response_upload.get('id'),
